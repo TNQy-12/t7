7257a82ed936b026116412c81d5b254971ad55c8
TNQy-12 <hoangtranthuylinh12072004@gmail.com>
2025-02-12 16:41:26 +0700

    vs12

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    my_variable = "Welcome to My Flask App!"
+    return render_template('index.html', variable=my_variable)
+
+if __name__ == '__main__':
+    app.run(debug=True)
